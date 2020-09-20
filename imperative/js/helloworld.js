65515a799dfb969a520ac1a3e52a686a86922d75
katychuang <katychuang@gmail.com>
2020-09-20 01:50:51 -0400

    add js and rust examples

---
+++ imperative/js/helloworld.js
@@ -0,0 +1,7 @@
+// Single line comment starts with //
+
+// variable assignment
+my_string="Hello World!";
+
+// printing output
+console.log(my_string);
---
+++ imperative/rust/helloworld.rs
@@ -0,0 +1,14 @@
+// single line comments start with //
+
+// main is the starting point for every executable Rust program (this is like c!)
+fn main() {
+
+    // declare a variable with the string
+    let my_string = "Hello World!";
+
+    // print output with the variable
+    println!("\n{}\n\n", my_string);
+    let a = "abc";
+    let b = &a;
+    println!("\n{}\n", b);
+}
